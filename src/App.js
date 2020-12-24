cc2057adc7e563c488391549a001a5d68bb87f24
SD170 <duttasaswata7@gmail.com>
2020-12-24 05:33:43 +0530

    base game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GameBox from './components/GameBox';
 
 function App() {
   return (
     <div className="App">
-     
+      <GameBox/>
     </div>
   );
 }
--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -1,26 +1,55 @@
 import React, { useState } from 'react';
 
 const n=3; //3x3 matrix/tic-tac-toe board
-const [boxMatrix, setBoxMatrix] = useState([
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
-])
-
-const winnerCheck = () =>{
-    if(boxMatrix[j] === boxMatrix[j+1] === boxMatrix[j+2]){
-        console.log(`${boxMatrix[j]} wins`;
-    }
-
-    // for(let i=0; i<n; i++){
-    //     for(let j=0; j<n; j++){
-    //     }
-    // }
-}
 
 const GameBox = () => {
+    const [boxMatrix, setBoxMatrix] = useState([
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
+    ])
+
+
+    const winnerCheck = () =>{
+
+        for(let i=0; i<n; i++){
+            for(let j=0; j<n; j++){
+                try{
+                    if( (new Set([boxMatrix[i][j],boxMatrix[i][j+1],boxMatrix[i][j+2]])).size === 1 && boxMatrix[i][j] != null &&  typeof(boxMatrix[i][j+2]) !== 'undefined'){
+                        console.log(`${boxMatrix[i][j]} wins`);
+                        console.log(boxMatrix[i][j]);
+                        return boxMatrix[i][j];
+                    }
+                    if((new Set([boxMatrix[i][j],boxMatrix[i+1][j],boxMatrix[i+2][j]])).size === 1 && boxMatrix[i][j] != null &&  typeof(boxMatrix[i+2][j]) !== 'undefined'){
+                        console.log(`${boxMatrix[i][j]} wins`);
+                        console.log(boxMatrix[i][j]);
+                        return boxMatrix[i][j];
+                    }
+                    if((new Set([boxMatrix[i][i],boxMatrix[i+1][i+1],boxMatrix[i+2][i+2]])).size === 1 && boxMatrix[i][i] != null &&  typeof(boxMatrix[i+2][i+2]) !== 'undefined'){
+                        console.log(`${boxMatrix[i][j]} wins`);
+                        console.log(boxMatrix[i][j]);
+                        return boxMatrix[i][j];
+                    }                    
+                    if((new Set([boxMatrix[i+2][i],boxMatrix[i+1][i+1],boxMatrix[i+2][i]])).size === 1 && boxMatrix[i][j] != null &&  typeof(boxMatrix[i+2][i+2]) !== 'undefined'){
+                        console.log(`${boxMatrix[i][j]} wins`);
+                        console.log(boxMatrix[i][j]);
+                        return boxMatrix[i][j];
+                    }
+                   
+                }catch{
+                    //do nothing                    
+                }
+                
+            }
+        }
+    }
+
+
+    winnerCheck();
     return (
-        winnerCheck
+        <div className="game-box">
+
+        </div>
     );
 }
  
