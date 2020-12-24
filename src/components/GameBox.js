36d2b891bfb0a557a1a8c839ef9b4436c8e7fb95
SD170 <duttasaswata7@gmail.com>
2020-12-24 21:10:19 +0530

    final 1.0

--- src/components/GameBox.js
+++ src/components/GameBox.js
@@ -24,6 +24,8 @@ const GameBox = () => {
             [null,null,null],
             [null,null,null]
         ])
+
+        window.location.reload();
     }
 
 
@@ -33,35 +35,33 @@ const GameBox = () => {
             for(let j=0; j<n; j++){
                 try{
                     if( (new Set([boxMatrix[i][j],boxMatrix[i][j+1],boxMatrix[i][j+2]])).size === 1 && boxMatrix[i][j] != null &&  typeof(boxMatrix[i][j+2]) !== 'undefined'){
-                        console.log(`${boxMatrix[i][j]} wins 1`);
-                        console.log(boxMatrix[i][j]);
-                        let res = boxMatrix[i][j];
+                        // console.log(`${boxMatrix[i][j]} wins 1`);
+                        alert(`${boxMatrix[i][j]} wins the game`);
                         resetGame();
-                        console.log(boxMatrix);
-                        return res;
+                        return;
                     }
                     if((new Set([boxMatrix[i][j],boxMatrix[i+1][j],boxMatrix[i+2][j]])).size === 1 && boxMatrix[i][j] != null &&  typeof(boxMatrix[i+2][j]) !== 'undefined'){
-                        console.log(`${boxMatrix[i][j]} wins 2`);
-                        console.log(boxMatrix[i][j]);
-                        let res=boxMatrix[i][j];
+                        // console.log(`${boxMatrix[i][j]} wins 2`);
+                        alert(`${boxMatrix[i][j]} wins the game`);
                         resetGame();
-                        console.log(boxMatrix);
-                        return res;
+                        return;
                     }
                     if((new Set([boxMatrix[i][i],boxMatrix[i+1][i+1],boxMatrix[i+2][i+2]])).size === 1 && boxMatrix[i][i] != null &&  typeof(boxMatrix[i+2][i+2]) !== 'undefined'){
-                        console.log(`${boxMatrix[i][j]} wins 3`);
-                        console.log(boxMatrix[i][j]);
-                        let res = boxMatrix[i][i];
+                        // console.log(`${boxMatrix[i][j]} wins 3`);
+                        alert(`${boxMatrix[i][j]} wins the game`);
                         resetGame();
-                        console.log(boxMatrix);
-                        return res;
+                        return;
                     }                    
                     if((new Set([boxMatrix[i+2][i],boxMatrix[i+1][i+1],boxMatrix[i][i+2]])).size === 1 && boxMatrix[i+1][i+1] != null &&  typeof(boxMatrix[i+2][i+2]) !== 'undefined'){
-                        console.log(`${boxMatrix[i+1][i+1]} wins 4`);
-                        let res = boxMatrix[i+1][i+1];
+                        // console.log(`${boxMatrix[i+1][i+1]} wins 4`);
+                        alert(`${boxMatrix[i][j]} wins the game`);
                         resetGame();
-                        console.log(boxMatrix);
-                        return res;
+                        return;
+                    }
+                    if(!boxMatrix[i].includes(null) && !boxMatrix[i+1].includes(null) && !boxMatrix[i+2].includes(null)){
+                        alert(`Game was a draw`);
+                        resetGame();
+                        return;
                     }
                    
                 }catch{
@@ -72,13 +72,11 @@ const GameBox = () => {
         }
     }
 
-
     winnerCheck();
 
     const onGameClick =(e)=>{
         const tempArray = boxMatrix;
-        tempArray[parseInt(e.target.id%10)-1][parseInt(e.target.id/10)-1] = currentSymbol;
-        console.log(boxMatrix[parseInt(e.target.id%10)-1][parseInt(e.target.id/10)-1])
+        tempArray[parseInt(e.target.id/10)-1][parseInt(e.target.id%10)-1] = currentSymbol;
         e.target.className = currentSymbol==='x'?'x icon huge icon':'circle outline huge icon';
         setBoxMatrix(tempArray);
         toggleSymbol();
@@ -87,24 +85,23 @@ const GameBox = () => {
     }
 
     return (
-        <div className="game-box">
-            <div className="ui centered grid">
-                <div className="column">
-                    <div className="ui three column centered grid">
-                        <div className="one wide column"><div className="content border-1"><i id='11' onClick={onGameClick}  className="huge icon"></i></div></div>
-                        <div className="one wide column"><div className="content border-1"><i id='12' onClick={onGameClick}  className="huge icon"></i></div></div>
-                        <div className="one wide column"><div className="content border-1"><i id='13' onClick={onGameClick}  className="huge icon"></i></div></div>
-                    </div>
-                    <div className="ui three column centered grid">
-                        <div className="one wide column"><div className="content border-1"><i id='21' onClick={onGameClick}  className="huge icon"></i></div></div>
-                        <div className="one wide column"><div className="content border-1"><i id='22' onClick={onGameClick}  className="huge icon"></i></div></div>
-                        <div className="one wide column"><div className="content border-1"><i id='23' onClick={onGameClick}  className="huge icon"></i></div></div>
-                    </div>
-                    <div className="ui three column centered grid">
-                        <div className="one wide column"><div className="content border-1"><i id='31' onClick={onGameClick}  className="huge icon"></i></div></div>
-                        <div className="one wide column"><div className="content border-1"><i id='32' onClick={onGameClick}  className="huge icon"></i></div></div>
-                        <div className="one wide column"><div className="content border-1"><i id='33' onClick={onGameClick}  className="huge icon"></i></div></div>
-                    </div>
+        <div className="center game-box">
+            <h1 style={{textAlign:'center'}}>Tic-Tac-Toe</h1>
+            <div className="grid">
+                <div className="row">
+                    <div className="cell"><i id='11' onClick={onGameClick}  className="huge icon"></i></div>
+                    <div className="cell"><i id='12' onClick={onGameClick}  className="huge icon"></i></div>
+                    <div className="cell"><i id='13' onClick={onGameClick}  className="huge icon"></i></div>
+                </div>
+                <div className="row">
+                    <div className="cell"><i id='21' onClick={onGameClick}  className="huge icon"></i></div>
+                    <div className="cell"><i id='22' onClick={onGameClick}  className="huge icon"></i></div>
+                    <div className="cell"><i id='23' onClick={onGameClick}  className="huge icon"></i></div>
+                </div>
+                <div className="row">
+                    <div className="cell"><i id='31' onClick={onGameClick}  className="huge icon"></i></div>
+                    <div className="cell"><i id='32' onClick={onGameClick}  className="huge icon"></i></div>
+                    <div className="cell"><i id='33' onClick={onGameClick}  className="huge icon"></i></div>
                 </div>
             </div>
         </div>
